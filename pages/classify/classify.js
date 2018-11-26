8205366e4434561aaa437f276a7d885a297b2486
bookstore <2535545766@qq.com>
2018-11-26 17:03:55 +0800

    朱伟煌提交

--- pages/classify/classify.js
+++ pages/classify/classify.js
@@ -1,66 +1,354 @@
-// pages/classify/classify.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    books: [
+      {
+        book_id: 1,
+        book_name: "言情",
+        ishaveChild: true,
+        children:
+          [
+            {
+              child_id: 1,
+              name: '三体',
+              image: "http://tp.yiaedu.com/showimg.php?url=http://uploads.xuexila.com/allimg/1703/867-1F330164643.jpg"
+            },
+            {
+              child_id: 2,
+              name: '十二个明天',
+              image: "http://img.zcool.cn/community/015da9554971170000019ae9f43459.jpg@2o.jpg"
+            },
+            {
+              child_id: 3,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 4,
+              name: '',
+              image: ""
+            }
+          ]
+      },
+      {
+        book_id: 2,
+        book_name: "玄幻",
+        ishaveChild: true,
+        children:
+          [
+            {
+              child_id: 1,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 2,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 3,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 4,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 5,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 6,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 7,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 8,
+              name: '',
+              image: ""
+            }
+          ]
+      },
+      {
+        book_id: 3,
+        book_name: "都市",
+        ishaveChild: true,
+        children:
+          [
+            {
+              child_id: 1,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 2,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 3,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 4,
+              name: '',
+              image: ""
+            }
+          ]
+      },
+      {
+        book_id: 4,
+        book_name: "科学",
+        ishaveChild: true,
+        children:
+          [
+            {
+              child_id: 1,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 2,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 3,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 4,
+              name: '',
+              image: ""
+            }
+          ]
+      },
+      {
+        book_id: 5,
+        book_name: "竞技",
+        ishaveChild: true,
+        children:
+          [
+            {
+              child_id: 1,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 2,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 3,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 4,
+              name: '',
+              image: ""
+            }
+          ]
+      },
+      {
+        book_id: 6,
+        book_name: "军事",
+        ishaveChild: true,
+        children:
+          [
+            {
+              child_id: 1,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 2,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 3,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 4,
+              name: '',
+              image: ""
+            }
+          ]
+      },
+      {
+        book_id: 7,
+        book_name: "历史",
+        ishaveChild: true,
+        children:
+          [
+            {
+              child_id: 1,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 2,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 3,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 4,
+              name: '',
+              image: ""
+            }
+          ]
+      },
+      {
+        book_id: 8,
+        book_name: "游戏",
+        ishaveChild: true,
+        children:
+          [
+            {
+              child_id: 1,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 2,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 3,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 4,
+              name: '',
+              image: ""
+            }
+          ]
+      },
+      {
+        book_id: 9,
+        book_name: "武侠",
+        ishaveChild: true,
+        children:
+          [
+            {
+              child_id: 1,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 2,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 3,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 4,
+              name: '',
+              image: ""
+            }
+          ]
+      },
+      {
+        book_id: 10,
+        book_name: "奇幻",
+        ishaveChild: true,
+        children:
+          [
+            {
+              child_id: 1,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 2,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 3,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 4,
+              name: '',
+              image: ""
+            }
+          ]
+      },
+      {
+        book_id: 11,
+        book_name: "校园",
+        ishaveChild: true,
+        children:
+          [
+            {
+              child_id: 1,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 2,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 3,
+              name: '',
+              image: ""
+            },
+            {
+              child_id: 4,
+              name: '',
+              image: ""
+            }
+          ]
+      },
+    ],
+    curNum: 1,
+    curIndex: 0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //事件处理函数  
+  switchRightTab: function (e) {
+    // 获取item项的id，和数组的下标值  
+    let id = e.target.dataset.id,
+      index = parseInt(e.target.dataset.index);
+    // 把点击到的某一项，设为当前index  
+    this.setData({
+      curNum: id,
+      curIndex: index
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  //跳转
+  // onLoad:function(options){
+  //   wx.navigateTo({
+  //     url: 'detail/detail',
+  //   })
+  // }
 })
--- pages/more/more.js
+++ pages/more/more.js
@@ -1,18 +1,24 @@
 // pages/more/more.js
+var util = require('../../utils/util.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    list:[],
+    loadingpageNum: 1,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // console.log("接收到的参数是list=" + options.list);
+    // this.data.list = JSON.parse(options.list);
+    this.setData({
+      list: JSON.parse(options.list)
+    })
   },
 
   /**
@@ -46,15 +52,31 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  // onPullDownRefresh: function () {
+  // },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    let that = this;
+    that.data.loadingpageNum += 1;
+    util.getSearchBook(that.data.loadingpageNum, function (data) {
+      console.log("数据长度：" + data.length)
+      if (data.length > 0) {
+        let searchList = [];
+        searchList = that.data.list.concat(data);
+        that.setData({
+          list: searchList,
+          searchLoading: true
+        })
+      } else {
+        that.setData({
+          searchLoadingComplete: true, //把“没有数据”设为true，显示
+          searchLoading: false  //把"上拉加载"的变量设为false，隐藏
+        });
+      }
+    })
   },
 
   /**
