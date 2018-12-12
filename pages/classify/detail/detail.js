aeea4a0460a9ab1daf4459fca0b158340b260bc1
lzn <961136007@qq.com>
2018-12-12 14:20:53 +0800

    陆中南12.12

--- pages/classify/detail/detail.js
+++ pages/classify/detail/detail.js
@@ -6,6 +6,7 @@ Page({
       //图片的地址
     "http://img.zcool.cn/community/014565554b3814000001bf7232251d.jpg@1280w_1l_2o_100sh.png"
     ],
+    bookId:'',
     indicatorDots: true, //是否显示指示点
     autoplay: true, //是否自动切换
     interval: 3000, //自动切换时间间隔
@@ -13,8 +14,10 @@ Page({
     list: [],//动态页面
     item:[],//显示所有书
     out:[],//显示折扣
-    isCollected: false,//收藏
+    author:[],//显示一个或多个作者
     minusStatus:'disabled',//预览退出
+    isCollected:'',//收藏
+
   },
   //预览图片
   previewImage: function (e) {
@@ -29,26 +32,34 @@ Page({
 
   // 收藏事件
   handleCollection:function(e) {
-    let isCollected = !this.data.isCollected
-    wx.request({
-      url:app.URL + 'bookstore-mall/3/3/kindAdd',
-    })
-    this.setData({
-      isCollected
-    })
-    if (app.globalData.userInfo == null) {
-      wx.navigateTo({
-        url: '/pages/login/login',
-      })
-    } else {
-    wx.showToast({
-      title: isCollected ? '收藏成功' : '取消收藏',
-      icon: 'success'
-    });
-    }
-  },
+ 
+	   var that = this
+    // if (app.globalData.userInfo == null) {
+    //   wx.navigateTo({
+    //     url: '/pages/login/login',
+    //   })
+    // } else {
+    // var userId = app.globalData.userInfo.userId
+    // console.log("传入的数据:" + userId)
 
+    var userId = 2
+    var bookId = that.data.bookId
 
+     //判断收藏的状态
+    that.isCollector(userId,bookId,function(data){
+       if(data==false){
+         that.insertCollector(userId,bookId);//添加收藏
+         return;
+       }
+        else{
+         that.delCollector(userId,bookId);//取消收藏
+         return;
+        }
+     })
+    
+    },
+
+  
   addCar: function (e) {
     //将购物车数据添加到缓存
     var that = this
@@ -129,9 +140,10 @@ Page({
    */
   onLoad: function (options) {
     console.log('传入的数据' + options.bookId)
-
+    var bookId = options.bookId;
+    var userId=2;
+// 调用查询所有书籍信息
    var that = this;
-   
    wx.request({
      url: app.URL + 'bookstore-mall/' + options.bookId+'/allContext',
      data:{},
@@ -141,13 +153,15 @@ Page({
      success:function(res){
        console.log(res.data)
        that.setData({
-          list: res.data //设置数据
+          list: res.data, //设置数据
+         bookId: options.bookId
         })
       },
       fail: function (err) {
         console.log(err)
      }
     })
+    // 查询所有评论
     wx.request({
       url: app.URL + 'bookstore-mall/' + options.bookId + '/allContext/kindAll',
       data: {},
@@ -164,6 +178,7 @@ Page({
         console.log(err)
       }
     })
+    // 查询所有折扣和折扣后的价格
     wx.request({
       url: app.URL + 'bookstore-mall/selectActivity/' + options.bookId,
       data: {},
@@ -180,59 +195,100 @@ Page({
         console.log(err)
       }
     })
+    // 查询所有作者
+    wx.request({
+      url: app.URL + 'bookstore-mall/selectAllAuthor/' + options.bookId,
+      data: {},
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          author: res.data //设置数据
+        })
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
+    this.isCollector(userId,bookId,function(data){});
+   
   },
   
-  
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //判断数据库中的这本书是否被收藏
+  isCollector: function (userId, bookId,callback){
+   var that=this;
+   wx.request({
+     url: app.URL + 'bookstore-mall/' + userId + '/' + bookId + '/isExit',
+     data: {},
+     header: {
+       'content-type': 'application/json'
+     },
+     success: function (res) {
+       console.log("panduanshoucang :"+res.data)
+       console.log(res.data)
+       if (res.data == false) {
+      console.log("nishshabi")
+         callback(false);//返回false说明这本书没有被收藏 
+         that.setData({
+           isCollected: false
+         })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+         console.log("is:" + that.data.isCollected);
+       }else{
+         callback(true);
+         that.setData({
+           isCollected: true
+         })
+       }
+      
+     }
+   })
+ },
+//添加收藏到数据库中的方法
+  insertCollector: function (userId,bookId){
+  var that = this;
+  wx.request({
+    url: app.URL + 'bookstore-mall/' + userId + '/' + bookId + '/kindAdd',
+    data: {},
+    header: {
+      'content-type': 'application/json'
+    },
+    success: function (res) {
+      console.log(res.data)
+      if (res.data == 0) {
+        console.log("添加失败！");
+      }
+      else{
+        console.log("添加成功！");
+      }
+    }
+  })
+},
+//取消收藏的方法
+  delCollector: function ( userId,bookId) {
+    var that = this;
+    wx.request({
+      url: app.URL + 'bookstore-mall/delete/' + userId + '/' + bookId,
+      data: {},
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data.status)
+        if (res.data.status == true) {
+          console.log("取消成功！");
+        }
+        else {
+          console.log("取消收藏失败！");
+        }
+      }
+    })
   }
 
 
 })
 
 
+  
