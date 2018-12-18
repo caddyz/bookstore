de44c357c33569defd16cb22afb1707d61581ed6
2946574674 <2946571671@qq.com>
2018-12-18 09:30:36 +0800

    12.18李孝标

--- app.js
+++ app.js
@@ -46,6 +46,66 @@ App({
       }
     })
   },
+  
+  //当微信小程序进入后台运行时执行的方法
+  onHide:function(){
+    console.log("我是进入后台执行的方法" + JSON.stringify(this.globalData.userInfo));
+    var that = this;
+    if (this.globalData.userInfo!=null)
+      var carts = wx.getStorageSync("carts")//获取缓存的数据
+    console.log("取出的缓存数据：" + JSON.stringify(carts));
+    that.insetCart(carts)//将数据存入数据库中
+  },
+
+  //将商品存入购物车数据存入数据库的方法
+  insetCart: function (carts) {
+  var that=this;
+    var Carts = carts;
+    var userId = this.globalData.userInfo.userId;//获取用户的id
+   var url=this.URL;
+
+//将用户的id传输到后台
+    wx.request({
+      url: url + 'bookstore-mall/insertCartUserId/'+userId, //提交的网络地址
+      method: "GET",
+      dataType: "json",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        //--init data
+        if (res.data == true) {
+          console.log("添加成功！");
+          that.insertCarts(Carts);   
+        }
+      }
+    });
+   
+  },
+  //将用户数据添加到后台的方法
+  insertCarts: function (carts) {
+    var Carts = carts;
+    var url = this.URL;
+    console.log("Carts" + JSON.stringify(Carts));
+    //将用户数据传输到后台
+    wx.request({
+      url: url + 'bookstore-mall/insertCart', //提交的网络地址
+      method: "POST",
+      dataType: "json",
+      data: JSON.stringify(Carts),
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        //--init data
+        if (res.data == true) {
+          console.log("添加成功！");
+          wx.clearStorage("carts");//添加到购物车后清除缓存里的数据
+        }
+      }
+    });
+  },
+  
   globalData: {
     userInfo: null,
   }
